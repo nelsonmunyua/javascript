cbdf811eb8e646e66f42d2203084dc43a9b33cef
Nelson Munyua <nelsonmunyua8@gmail.com>
2025-09-16 12:35:25 +0300

    dom-manipulation

---
+++ dom-manipulation.js
@@ -0,0 +1,14 @@
+/**
+ * DOM --> (document object model) - a programming iterface for web documents.
+ * -->It represents the structure of HTML /XML documents as a tree of objects
+ * DOM-MANIPULATION -->
+ */
+// We are going to use the crud
+// CREATE
+// document.createElement("")
+
+const main = document.createElement("main");
+document.body.appendChild(main);
+// READ
+// UPDATE
+// DELETE
